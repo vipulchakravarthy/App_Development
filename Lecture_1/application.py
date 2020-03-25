08d150602f8250301185779758e5647a5c404d74
vipulchakravarthy <saivipul15@gmai.com>
2020-03-25 13:50:24 +0530

    conditional statements in html using jinja2

--- Lecture_1/application.py
+++ Lecture_1/application.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template # Import the class `Flask` from the `flask` module, written by someone else.
+import datetime
 
 app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
 
 @app.route("/") # A decorator; when the user goes to the route `/`, exceute the function immediately below
 def index():
-  name = "Vipul"
-  return render_template("index.html", Name=name)
+  now = datetime.datetime.now()
+  new_year = now.month == 1 and now.day == 1
+  return render_template("index.html", new_year = True)
