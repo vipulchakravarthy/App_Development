88d0f65ee39cfa8e5a732b91fa413c54f10b8613
vipulchakravarthy <saivipul15@gmai.com>
2020-03-25 14:48:22 +0530

    sessions

--- Lecture_1/application.py
+++ Lecture_1/application.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template, request # Import the class `Flask` from the `flask` module, written by someone else.
+from flask import Flask, render_template, request, session # Import the class `Flask` from the `flask` module, written by someone else.
 import datetime
+
+from flask_session import Session # an additional extension to sessions which allows them
+                                  # to be stored server-side
 
 app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
 
-@app.route("/") # A decorator; when the user goes to the route `/`, exceute the function immediately below
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-  return render_template("index.html")
+    if session.get("notes") is None:
+        session["notes"] = []
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+
+    return render_template("index.html", notes=session["notes"])
+
 
 @app.route("/more")
 def more():
-	return render_template("more.html")
+    return render_template("more.html")
 
 
 @app.route("/hello", methods=["POST"])
