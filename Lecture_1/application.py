94ab54c75bc7ac33f978ad946cb5331d7eed7d7c
vipulchakravarthy <saivipul15@gmai.com>
2020-03-25 13:37:05 +0530

    rendering html

--- Lecture_1/application.py
+++ Lecture_1/application.py
@@ -1,12 +1,8 @@
-from flask import Flask # Import the class `Flask` from the `flask` module, written by someone else.
+from flask import Flask, render_template # Import the class `Flask` from the `flask` module, written by someone else.
 
 app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
 
 @app.route("/") # A decorator; when the user goes to the route `/`, exceute the function immediately below
 def index():
-  return "Hello, world!"
-
-
-@app.route("/<string:name>")
-def hello(name):
-    return f"Hello, {name}!"
+  name = "Vipul"
+  return render_template("index.html", Name=name)
