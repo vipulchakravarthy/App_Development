6c9ad0c5c19fe29ec712b2e027f108ba2632de73
vipulchakravarthy <saivipul15@gmai.com>
2020-03-25 13:24:55 +0530

    flask hello_world

---
+++ Lecture_1/application.py
@@ -0,0 +1,7 @@
+from flask import Flask # Import the class `Flask` from the `flask` module, written by someone else.
+
+app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
+
+@app.route("/") # A decorator; when the user goes to the route `/`, exceute the function immediately below
+def index():
+  return "Hello, world!"
