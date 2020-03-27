27ec017b0e7dc24002e7f5bf77694d1fc02af2ae
vipulchakravarthy <saivipul15@gmai.com>
2020-03-27 16:39:17 +0530

    Allow authenticated user to home and logout

--- project1/application.py
+++ project1/application.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from models import *
+from flask_session import Session
 import os
 from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
+
+#Configure DB
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    if session.get("user_id") is None:
+        return render_template('register.html', text ="Please Login")
+    return render_template("home.html", text="Welcome to Book Reviews")
 
 @app.route("/register",methods=["GET","POST"])
 def register():
@@ -21,10 +30,6 @@ def register():
         email = request.form.get("email")
         password = request.form.get("password")
 
-        # data = User.query.filter_by(email=email).all()
-        # if len(data) > 0:
-        #     message = "User already exists"
-        #     
         try:
             new_user = User(username= user_name, email= email, password = password) 
             db.session.add(new_user)
@@ -45,9 +50,21 @@ def authentication():
 
     data = User.query.filter_by(email=email)
     if data[0].email == email and data[0].password == password:
+        session["user_id"] = data[0].id
+        session["user_name"] = data[0].username
         return render_template("home.html", text="Welcome to Book Reviews")
 
     return render_template("register.html", text="email or password is incorrect")
+
+@app.route("/logout")
+def logout():
+    """ Log user out """
+
+    # Forget any user ID
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/register")
 
 @app.route("/admin", methods=["GET"])
 def fetch_users():
