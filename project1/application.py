825002d99f1ce25d705894710b8ca46cb7660c49
vipulchakravarthy <saivipul15@gmai.com>
2020-03-28 11:49:57 +0530

    Fixed the issues

--- project1/application.py
+++ project1/application.py
@@ -1,28 +1,39 @@
+'''
+flask module to develop web Applciation
+'''
 from flask import Flask, render_template, request, session, redirect
 from models import *
 from flask_session import Session
 import os
 from sqlalchemy.exc import SQLAlchemyError
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 #Configure DB
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+APP.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+APP.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(APP)
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+APP.config["SESSION_PERMANENT"] = False
+APP.config["SESSION_TYPE"] = "filesystem"
+Session(APP)
 
-@app.route("/")
+'''
+This route is for home page
+check if the user is logged or not
+if not redirect to register
+'''
+@APP.route("/")
 def index():
     if session.get("user_id") is None:
-        return render_template('register.html', text ="Please Login")
+        return render_template('register.html', text="Please Login")
     return render_template("home.html", text="Welcome to Book Reviews")
 
-@app.route("/register",methods=["GET","POST"])
+'''
+This route is for register page
+'''
+@APP.route("/register", methods=["GET", "POST"])
 def register():
     message = ""
     if request.method == "POST":
@@ -31,19 +42,21 @@ def register():
         password = request.form.get("password")
 
         try:
-            new_user = User(username= user_name, email= email, password = password) 
+            new_user = User(username=user_name, email=email, password=password)
             db.session.add(new_user)
             db.session.commit()
             message = "Registered successfully, Please Login"
-            return render_template("success.html", text = message)
+            return render_template("success.html", text=message)
 
         except SQLAlchemyError as e:
             message = str(e.__dict__['orig'])
-            return render_template("success.html", text = message)
-        
+            return render_template("success.html", text=message)
     return render_template('register.html')
 
-@app.route("/auth", methods=["POST"])
+'''
+This route is for authentication
+'''
+@APP.route("/auth", methods=["POST"])
 def authentication():
     email = request.form.get("email")
     password = request.form.get("password")
@@ -56,17 +69,22 @@ def authentication():
 
     return render_template("register.html", text="email or password is incorrect")
 
-@app.route("/logout")
+'''
+This route is to logout
+'''
+@APP.route("/logout")
 def logout():
     """ Log user out """
-
     # Forget any user ID
     session.clear()
 
     # Redirect user to login form
     return redirect("/register")
 
-@app.route("/admin", methods=["GET"])
+'''
+This route is for printing all users
+'''
+@APP.route("/admin", methods=["GET"])
 def fetch_users():
-    data = User.query.all()
+    data = User.query.order_by(User.created.desc()).all()
     return render_template('users.html', data=data)
